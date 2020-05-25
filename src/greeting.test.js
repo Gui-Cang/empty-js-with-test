eb2097d83c29c3c3f2814dfd553e36d4d20dc8c9
Liu Xia <lxconan@gmail.com>
2020-05-25 11:25:07 +0800

    [liuxia] feat: initial commit.

---
+++ src/greeting.test.js
@@ -0,0 +1,8 @@
+// This is a sample. Please delete it.
+import say from './greeting';
+
+describe('for greeting', () => {
+  it('should say hello', () => {
+    expect(say()).toBe('Hello');
+  });
+});
